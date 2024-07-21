44000bf75406c406e0ca03f5f345d635434a3185
harikrishnan.vinod <harikrishnan.vinod@claysys.in>
2024-07-21 20:30:19 +0530

    stage 1 build

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 "use strict";
+const GetResult = () => {
+    let name1 = document.getElementById("FirstPerson").value;
+    let name2 = document.getElementById("SecondPerson").value;
+    name1 = name1.trim().toLocaleLowerCase();
+    name2 = name2.trim().toLocaleLowerCase();
+    let smallestName = name1.length < name2.length ? name1 : name2;
+    Array.from(smallestName).forEach((char) => {
+        if (name1.includes(char) && name2.includes(char)) {
+            name1 = name1.split(char).join("");
+            name2 = name2.split(char).join("");
+        }
+    });
+    const iterationCount = name1.length + name2.length;
+    let flames = ["F", "L", "A", "M", "E", "S"];
+    while (flames.length > 1) {
+        const index = iterationCount % flames.length;
+        if (index === 0) {
+            flames.pop();
+        }
+        else {
+            flames.splice(index - 1, 1);
+        }
+    }
+    const resultObj = {
+        F: "Friends",
+        L: "Lovers",
+        A: "Affectionate",
+        M: "Marriage",
+        E: "Enemies",
+        S: "Siblings",
+    };
+    alert(`You two are ${resultObj[flames[0]]}!`);
+};
