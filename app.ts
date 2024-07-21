617fb54932b9204d9eb584b55f632ab2769a63ec
harikrishnan.vinod <harikrishnan.vinod@claysys.in>
2024-07-21 20:39:43 +0530

    Included null check

--- app.ts
+++ app.ts
@@ -8,36 +8,43 @@ const GetResult = (): void => {
 
   name1 = name1.trim().toLocaleLowerCase();
   name2 = name2.trim().toLocaleLowerCase();
-  let smallestName: string = name1.length < name2.length ? name1 : name2;
 
-  Array.from(smallestName).forEach((char) => {
-    if (name1.includes(char) && name2.includes(char)) {
-      name1 = name1.split(char).join("");
-      name2 = name2.split(char).join("");
+  if (name1.length == 0) {
+    alert("First person name is empty !");
+  } else if (name1.length == 0) {
+    alert("Second person name is empty!");
+  } else {
+    let smallestName: string = name1.length < name2.length ? name1 : name2;
+
+    Array.from(smallestName).forEach((char) => {
+      if (name1.includes(char) && name2.includes(char)) {
+        name1 = name1.split(char).join("");
+        name2 = name2.split(char).join("");
+      }
+    });
+
+    const iterationCount = name1.length + name2.length;
+
+    let flames = ["F", "L", "A", "M", "E", "S"];
+
+    while (flames.length > 1) {
+      const index = iterationCount % flames.length;
+      if (index === 0) {
+        flames.pop();
+      } else {
+        flames.splice(index - 1, 1);
+      }
     }
-  });
 
-  const iterationCount = name1.length + name2.length;
+    const resultObj: { [key: string]: string } = {
+      F: "Friends",
+      L: "Lovers",
+      A: "Affectionate",
+      M: "Marriage",
+      E: "Enemies",
+      S: "Siblings",
+    };
 
-  let flames = ["F", "L", "A", "M", "E", "S"];
-
-  while (flames.length > 1) {
-    const index = iterationCount % flames.length;
-    if (index === 0) {
-      flames.pop();
-    } else {
-      flames.splice(index - 1, 1);
-    }
+    alert(`You two are ${resultObj[flames[0]]}!`);
   }
-
-  const resultObj: { [key: string]: string } = {
-    F: "Friends",
-    L: "Lovers",
-    A: "Affectionate",
-    M: "Marriage",
-    E: "Enemies",
-    S: "Siblings",
-  };
-
-  alert(`You two are ${resultObj[flames[0]]}!`);
 };
